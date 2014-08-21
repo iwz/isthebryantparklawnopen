e0d90c32e6a3b3fc958c21ba8c946f2e4c80e67e
Ian Zabel <ian@thoughtbot.com>
2014-08-21 08:45:50 -0400

    add lawn messages

--- application.rb
+++ application.rb
@@ -12,6 +12,12 @@ Capybara.current_driver = :webkit
 Capybara.app_host = "http://bryantpark.org"
 
 LAWN_OPEN_MESSAGES = ["The lawn is open for your enjoyment."]
+LAWN_CLOSED_MESSAGES = [
+  "The lawn will open at 5pm for the HBO Summer Film Festival.",
+  "The lawn will open at 4:30pm for your enjoyment.",
+  "The lawn is resting after the HBO Bryant Park Film Festival.",
+  "The lawn will open at 12:00pm for your enjoyment.",
+]
 
 get "/" do
   visit "/"
