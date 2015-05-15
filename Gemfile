55016c1d77be9aafb2268a4b41de7355916803c6
Ian Zabel <ian@thoughtbot.com>
2015-05-15 09:54:55 -0400

    Switch from `capybara` to `nokogiri`

--- Gemfile
+++ Gemfile
@@ -1,6 +1,6 @@
 source "https://rubygems.org"
 
-gem "capybara"
-gem "capybara-webkit"
+gem "nokogiri"
 gem "sinatra"
 gem "slim"
+gem "pry"
--- application.rb
+++ application.rb
@@ -1,15 +1,8 @@
 require "sinatra"
-require "capybara"
-require "capybara/dsl"
-require "capybara-webkit"
 require "slim"
-
-include Capybara::DSL
-
-Capybara.app = Sinatra::Application
-Capybara.run_server = false
-Capybara.current_driver = :webkit
-Capybara.app_host = "http://bryantpark.org"
+require 'nokogiri'
+require 'open-uri'
+require 'pry'
 
 LAWN_OPEN_MESSAGES = ["The lawn is open for your enjoyment."]
 LAWN_CLOSED_MESSAGES = [
@@ -17,19 +10,18 @@ LAWN_CLOSED_MESSAGES = [
   "The lawn will open at 4:30pm for your enjoyment.",
   "The lawn is resting after the HBO Bryant Park Film Festival.",
   "The lawn will open at 12:00pm for your enjoyment.",
+  "The lawn is closed until 1PM on Friday for mowing.",
 ]
 
 get "/" do
-  visit "/"
+  page = Nokogiri::HTML(open('http://www.bryantpark.org/'))
 
-  within "#today_in_the_park" do
-    lawn_message = find("ul:nth-child(2) > li:nth-child(2)").text
+  @lawn_message = page.css("#today_in_the_park ul:nth-child(2) > li:nth-child(2)").text.strip
 
-    if LAWN_OPEN_MESSAGES.include? lawn_message
-      @open = "Yes"
-    else
-      @open = "No"
-    end
+  if LAWN_OPEN_MESSAGES.include? @lawn_message
+    @open = "Yes"
+  else
+    @open = "No"
   end
 
   slim :index
