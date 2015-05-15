4a3ccaab5f0c46816736526ee09bac9639b110e3
Ian Zabel <ian@thoughtbot.com>
2015-05-15 15:18:38 -0400

    Add /api json api

--- application.rb
+++ application.rb
@@ -4,6 +4,7 @@ require "nokogiri"
 require "open-uri"
 require "sass"
 require "pry"
+require "json"
 
 LAWN_OPEN_MESSAGES = ["The lawn is open for your enjoyment."]
 LAWN_CLOSED_MESSAGES = [
@@ -14,12 +15,37 @@ LAWN_CLOSED_MESSAGES = [
   "The lawn is closed until 1PM on Friday for mowing.",
 ]
 
+class Lawn
+  def initialize
+    @page = Nokogiri::HTML(open('http://www.bryantpark.org/'))
+  end
+
+  def message
+    page.css("#today_in_the_park ul:nth-child(2) > li:nth-child(2)").text.strip
+  end
+
+  def open?
+    LAWN_OPEN_MESSAGES.include? message
+  end
+
+  def to_json
+    {
+      open: open?,
+      message: message,
+      timestamp: Time.now
+    }.to_json
+  end
+
+  private
+
+  attr_reader :page
+end
+
 get "/" do
-  page = Nokogiri::HTML(open('http://www.bryantpark.org/'))
+  lawn = Lawn.new
+  @lawn_message = lawn.message
 
-  @lawn_message = page.css("#today_in_the_park ul:nth-child(2) > li:nth-child(2)").text.strip
-
-  if LAWN_OPEN_MESSAGES.include? @lawn_message
+  if lawn.open?
     @open = "Yes"
   else
     @open = "No"
@@ -28,6 +54,14 @@ get "/" do
   slim :index
 end
 
+get "/api" do
+  content_type :json
+
+  Lawn.new.to_json
+end
+
 get "/stylesheets/:name.css" do
   scss :"/stylesheets/#{params[:name]}"
 end
+
+
