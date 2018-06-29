ed4f5fcf48473db892beeee32166a4341f204269
Ian Zabel <ian@thoughtbot.com>
2018-06-29 15:26:22 -0400

    stubbing

--- spec/features/bryant_park_lawn_spec.rb
+++ spec/features/bryant_park_lawn_spec.rb
@@ -2,13 +2,27 @@ require "spec_helper"
 
 RSpec.describe "Bryant Park Lawn status", :type => :feature do
 
+  def stub_bryant_park_api_fetch(lawn_status)
+    allow(BryantParkApi).to receive(:json).and_return(
+      {
+        "page": {
+          "lawnStatus": lawn_status
+        }
+      }
+    )
+  end
+
   it "when the lawn is closed" do
+    stub_bryant_park_api_fetch( "The Lawn is closed")
+
     visit "/"
 
     expect(page).to have_text("No")
   end
 
   it "when the lawn is open" do
+    stub_bryant_park_api_fetch( "The Lawn is open")
+
     visit "/"
 
     expect(page).to have_text("Yes")
